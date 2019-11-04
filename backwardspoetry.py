d1c714dfe1b760fdc0e8d35daf72963b72cc8ec3
asha952 <ashabsiddiqui952@gmail.com>
2019-11-03 22:06:08 -0800

    lines_printed_backwards in progress

--- backwardspoetry.py
+++ backwardspoetry.py
@@ -1,4 +1,4 @@
-print("hello world")
+# print("hello world")
 
 poem = """ I like potatoes
 They are interesting
@@ -14,8 +14,17 @@ backwardsPoem = """
 1 I like potatoes
 """
 split_poem = poem.split('\n')
+length_list = len(split_poem)
+
 
 def lines_printed_backwards(fragments):
+    a = []
     for lines in reversed(split_poem):
+        # i = length_list
+        a.append(lines)
+        while number > 0:
+            print(number + a)
+            number = number - 1
 
-    print()
+
+lines_printed_backwards(split_poem)
