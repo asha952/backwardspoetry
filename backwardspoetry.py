1ce8cc614a6fcd064ec2aa4d4e12cb4b3b8c203b
asha952 <ashabsiddiqui952@gmail.com>
2019-11-03 21:45:50 -0800

    setup

--- backwardspoetry.py
+++ backwardspoetry.py
@@ -1,1 +1,21 @@
 print("hello world")
+
+poem = """ I like potatoes
+They are interesting
+Also they are versatile
+You can make fries
+Or even mashed potatoes"""
+
+backwardsPoem = """
+5 Or even mashed potatoes
+4 You can make fries
+3 Also they are versatile
+2 They are interesting
+1 I like potatoes
+"""
+split_poem = poem.split('\n')
+
+def lines_printed_backwards(fragments):
+    for lines in reversed(split_poem):
+
+    print()
